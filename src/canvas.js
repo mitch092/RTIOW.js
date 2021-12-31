449e4f9cec21366d7f4acd49c49990b832d499ee
mitch092 <stevenm7861@gmail.com>
2021-12-31 02:56:54 -0800

    Refactored fill context.

--- src/canvas.js
+++ src/canvas.js
@@ -12,21 +12,21 @@ const addCanvas = (rect, name) => {
 
 const contextFillStyle = color => `rgba(${color.getRed()},${color.getGreen()},${color.getBlue()},${color.getAlpha()})`
 
-const fillContext = context => rect => color => {
+const fillContext = context => color => lowerRight => {
     context.fillStyle = contextFillStyle(color)
-    context.fillRect(0, 0, rect.getLowerRight().getX(), rect.getLowerRight().getY())
+    context.fillRect(0, 0, lowerRight.getX(), lowerRight.getY())
 }
 
-const setOnePixel = context => color => vec2d => {
+const setOnePixel = context => color => pixelCoord => {
     context.fillStyle = contextFillStyle(color)
-    context.fillRect(vec2d.getX(), vec2d.getY(), 1, 1)
+    context.fillRect(pixelCoord.getX(), pixelCoord.getY(), 1, 1)
 }
 
 const create = (rect, name = "canvas") => {
     const canvas = addCanvas(rect, name)
     const context = canvas.getContext("2d");
     return {
-        fill: (color) => fillContext(context)(rect)(color),
+        fill: (color) => fillContext(context)(color)(rect.getLowerRight()),
         setPixel: (vec2d, color) => setOnePixel(context)(color)(vec2d),
         getWidth: () => rect.getLowerRight().getX(),
         getHeight: () => rect.getLowerRight().getY()
